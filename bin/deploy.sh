c20362bdb5e4445f2711146f1b9bf103f293ec56
arfullight <tmimvhuang@gmail.com>
2020-11-07 11:48:05 -0700

    update shellscript to use staging as default and is able to deploy a different branch

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,15 @@
 HOME_DIR=/home/jupyterteam
+BRANCH=staging
+
+if [ $# -eq 1 ]
+then
+  BRANCH=$1
+fi
 
 ssh -tt jupyterteam@test.libretexts.org << EOF
 cd $HOME_DIR/ckeditor-query-plugin
 git fetch --all
-git checkout main && git reset --hard origin/main
+git checkout $BRANCH && git reset --hard origin/$BRANCH
 yarn install
 yarn build
 cp dist/queryPlugin.min.js $HOME_DIR/public
