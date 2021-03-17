11ddef4973bfb19979db5b65543d443e4ea0263d
Tim <timstewartj@gmail.com>
2021-03-17 11:38:28 -0700

    very rudimentary test code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,8 +6,10 @@ import '../styles/index.scss';
 import './config';
 import 'ckeditor4';
 import registerPlugin from './plugin';
+import registerAdapt from './plugindos';
 
 require('../index.html');
 registerPlugin();
+registerAdapt();
 CKEDITOR.replace('editor');
 
