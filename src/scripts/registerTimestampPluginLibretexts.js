9cd0d69b75e7391ee5d2ed40e367e359e923b1fb
Yasin Dahi <ymdahi@gmail.com>
2021-12-17 14:56:52 -0500

    build

---
+++ src/scripts/registerTimestampPluginLibretexts.js
@@ -0,0 +1,15 @@
+/*
+ * This is the production build
+ */
+import '../styles/index.scss';
+import registerPlugin from './plugin';
+
+// Adds this plugin to the LibreEditor for later activation
+// this will ensure that `registerPlugin()` be called after
+// CKeditor is initialized on Libretexts page
+LibreEditor.ydahiPlugin = (config) => {
+  registerPlugin();
+  config.toolbar[13].push('Timestamp');
+  //config.toolbar[12].push('openLibretextsAdaptDialog');
+};
+
